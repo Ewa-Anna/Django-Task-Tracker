c668544893b3df9658b00a773063a74eff863867
HurasAdam <huradam@wp.pl>
2024-01-12 21:38:23 +0100

    Add Topbar,LeftSidebar and Bottombar componnts to RootLayout structure. Add getUsers and logoutUser to user-api.

--- frontend/src/_root/RootLayout.tsx
+++ frontend/src/_root/RootLayout.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import Bottombar from "@/components/ui/shared/Bottombar";
+import LeftSidebar from "@/components/ui/shared/LeftSidebar";
+import Topbar from "@/components/ui/shared/Topbar";
+import { Outlet } from "react-router-dom";
+
+
 
 const RootLayout = () => {
   return (
-    <div>RootLayout</div>
-  )
-}
+    <div className="w-full md:flex">
+      <Topbar />
+      <LeftSidebar />
 
-export default RootLayout
+      <section className="flex flex-1 h-full">
+        <Outlet />
+      </section>
+
+      <Bottombar />
+    </div>
+  );
+};
+
+export default RootLayout;
--- frontend/src/features/user-api/user-api.ts
+++ frontend/src/features/user-api/user-api.ts
@@ -1,28 +1,35 @@
+import { RegisterFormData } from "../../components/pages/register/Register";
+import { SignInFormData } from "../../components/pages/signIn/SignIn";
+import axios from "../axios/axios";
+import clientApi from "../axios/axios";
 
-import {RegisterFormData} from "../../components/pages/register/Register"
-import {SignInFormData} from "../../components/pages/signIn/SignIn"
-import axios from "../axios/axios"
-import clientApi from "../axios/axios"
+export const createUserAccount = async (formData: RegisterFormData) => {
+  const response = await axios.post("user/register/", formData);
+  return response.data;
+};
 
+export const loginUser = async (formData: SignInFormData) => {
+  try {
+    const response = await clientApi.post("/user/login/", formData);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+export const logoutUser = async () => {
+  try {
+    const response = await clientApi.post("/user/logout/");
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
- export const createUserAccount = async(formData:RegisterFormData)=>{
-
-const response = await axios.post("user/register/",formData)
-return response.data
-}
-
-
- export const loginUser= async (formData:SignInFormData)=>{
-
-    try{
-        const response = await clientApi.post("/user/login/",formData)
-        return response.data
-    }
-
-    catch(error){
-        console.error(error);
-  
-    }
-
-}
-
+export const getUsers = async () => {
+  try {
+    const response = await clientApi.get("/user/users/");
+    return response.data();
+  } catch (error) {
+    console.log(error);
+  }
+};
