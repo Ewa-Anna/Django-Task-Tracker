a53635239aa72c368ae699e2f7a354b6d76b35e2
HurasAdam <huradam@wp.pl>
2024-01-13 21:54:46 +0100

    Add PostCard, Select and Card components.

--- frontend/src/components/ui/select.tsx
+++ frontend/src/components/ui/select.tsx
@@ -86,7 +86,7 @@ const SelectContent = React.forwardRef<
       <SelectScrollUpButton />
       <SelectPrimitive.Viewport
         className={cn(
-          "p-1",
+          "p-1 shad-input",
           position === "popper" &&
             "h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)]"
         )}
