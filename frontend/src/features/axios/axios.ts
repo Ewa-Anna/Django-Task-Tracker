72025f58b632204b855291d5a027c67d44da307c
HurasAdam <huradam@wp.pl>
2024-01-18 22:47:20 +0100

    feat: Add axios header config to handle CSRF token.

--- frontend/src/features/axios/axios.ts
+++ frontend/src/features/axios/axios.ts
@@ -1,10 +1,18 @@
 import axios from "axios";
+axios.defaults.xsrfCookieName = 'csrftoken'
+axios.defaults.xsrfHeaderName = "X-CSRFTOKEN"
+
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
+
+
 
 const clientApi = axios.create({
   baseURL: API_BASE_URL,
   withCredentials: true,
+  headers: {
+    'X-CSRFToken': "BAMDqaLoAYESRmZCZdybcNyru6j18XP4"
+  },
 });
 
 export default clientApi;
