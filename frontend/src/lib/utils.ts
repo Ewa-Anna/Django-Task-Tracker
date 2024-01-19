32ec23c1c452e47dea77242633a18196be427088
HurasAdam <huradam@wp.pl>
2024-01-19 23:20:24 +0100

    Add formatTimestamp function in utils file.

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -4,3 +4,28 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const formatTimestamp = (timestamp:string) => {
+  const date = new Date(timestamp);
+
+  const monthNamesShort = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  const formattedDate = `${
+    monthNamesShort[date.getMonth()]
+  } ${date.getDate()}, ${date.getFullYear()}`;
+
+  return formattedDate;
+};
