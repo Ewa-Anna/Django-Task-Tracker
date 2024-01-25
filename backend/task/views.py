b5a7bfde61ea35b279676f7b2ef68c952ecc1b07
Ewa-Anna <ewaanna073@gmail.com>
2024-01-25 20:12:21 +0100

    Formatting

--- backend/task/views.py
+++ backend/task/views.py
@@ -10,7 +10,7 @@ from user.models import ROLES, THEMES
 
 class DictionaryContentView(APIView):
     permission_classes = [AllowAny]
-    
+
     def get(self, request, dictionary_name):
         if dictionary_name == "priority":
             content = dict(PRIORITY)
@@ -33,9 +33,7 @@ class DictionaryContentView(APIView):
         )
         serializer.is_valid(raise_exception=True)
 
-        serialized_data = [
-            value for _, value in content.items()
-        ]
+        serialized_data = [value for _, value in content.items()]
 
         return Response(serialized_data, status=status.HTTP_200_OK)
 
