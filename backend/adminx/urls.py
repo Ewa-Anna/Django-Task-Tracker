999006f276f5cd26336f714adb684e141ec9c930
Ewa-Anna <ewaanna073@gmail.com>
2024-01-29 16:33:25 +0100

    Added Contact Form functionality

--- backend/adminx/urls.py
+++ backend/adminx/urls.py
@@ -1,9 +1,10 @@
 from django.urls.conf import path
 
-from .views import ChangeLogView
+from .views import ChangeLogView, ContactFormView
 
 app_name = "adminx"
 
 urlpatterns = [
     path("", ChangeLogView.as_view(), name="change_log_view"),
+    path("contact/", ContactFormView.as_view(), name="contact_form"),
 ]
