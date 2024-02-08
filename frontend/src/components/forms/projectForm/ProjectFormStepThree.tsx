f4e4f78fb20588e78a03056dd1eabe37b6288037
HurasAdam <huradam@wp.pl>
2024-02-09 00:00:23 +0100

    Add tag selection functionality to ProjectFormStepThree.

--- frontend/src/components/forms/projectForm/ProjectFormStepThree.tsx
+++ frontend/src/components/forms/projectForm/ProjectFormStepThree.tsx
@@ -1,6 +1,7 @@
 import {
   Form,
   FormControl,
+  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -24,12 +25,14 @@ import { ProjectStepThreeValidation } from "@/lib/validation";
 import { Button } from "@/components/ui/button";
 import { ArrowLeft, ArrowRight, ArrowRightLeft } from "lucide-react";
 import { useAppState } from "@/contexts/formContext";
+import { useQuery } from "react-query";
+import { RadioGroup, RadioGroupItem } from "@radix-ui/react-radio-group";
+import { Checkbox } from "@/components/ui/checkbox";
+import { useState } from "react";
 
-const ProjectFormStepThree = ({ currentStep, setCurrentStep }) => {
-
-
-
-const [state,setState]=useAppState()
+const ProjectFormStepThree = ({ currentStep, setCurrentStep, tags }) => {
+  const [state, setState] = useAppState();
+  const [selectedTags,setSelectedTags]=useState([])
 
   const form = useForm<z.infer<typeof ProjectStepThreeValidation>>({
     resolver: zodResolver(ProjectStepThreeValidation),
@@ -41,22 +44,13 @@ const [state,setState]=useAppState()
     },
   });
 
-
-
   const processForm = (data) => {
     setState({ ...state, ...data });
-    
-    setCurrentStep(prev=>prev+1)
-  
+
+    setCurrentStep((prev) => prev + 1);
   };
 
-
-
-
-
-
-
-
+  console.log(tags && tags);
 
   return (
     <div
@@ -71,10 +65,11 @@ const [state,setState]=useAppState()
             currentStep === 5,
         }
       )}
-    > 
+    >
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(processForm)}
-        className="flex flex-col justify-between  min-h-[620px] max-h-[620px]"
+        <form
+          onSubmit={form.handleSubmit(processForm)}
+          className="flex flex-col justify-between  min-h-[620px] max-h-[620px]"
         >
           <div className="flex flex-col gap-10 ">
             <FormField
@@ -92,50 +87,65 @@ const [state,setState]=useAppState()
               )}
             />
 
-            <FormField
-              control={form.control}
-              name="tags"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className="shad-form_label">
-                    Add Tags (separated y comma " , "
-                  </FormLabel>
-                  <FormControl>
-                    <Input
-                      placeholder="JS, React, NextJS, Node"
-                      type="text"
-                      className="shad-input"
-                      {...field}
-                      onChange={(e) => {
-                        const inputValue = e.target.value;
-                        const tagsArray = inputValue
-                          .split(",")
-                          .map((tag) => tag.trim())
-                          .filter((tag) => tag !== ""); // Remove empty tags
+            {/* aas */}
+           
+            <div className="flex-1   ">
+              <FormField
+                control={form.control}
+                name="tags"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Tags</FormLabel>
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger className="shad-input">
+                          <SelectValue 
+                   
+                          placeholder={form.watch("owner")} />
+                        </SelectTrigger>
+                      </FormControl>
+                      <FormMessage className="shad-form_message" />
+                   
+                      <SelectContent>
+                      <SelectGroup>
+                        {tags &&
+                          tags?.results.map((tag) => {
+                            return (
+                              <SelectItem
+                                className="cursor-pointer "
+                                value={tag.id.toString()}
 
-                        field.onChange(tagsArray);
-                      }}
-                    />
-                  </FormControl>
+                                
+                              >
+                                {tag?.name}
+                              </SelectItem>
+                            );
+                          })}
+                                </SelectGroup>  
+                      </SelectContent>
+                    </Select>
+                  </FormItem>
+                )}
+              />
+            </div>
 
-                  <FormMessage className="shad-form_message" />
-                </FormItem>
-              )}
-            />
+            {/* sad */}
           </div>
           <div className="flex justify-end ">
-            <Button 
-            className=" hover:bg-slate-800 " variant="ghost"
-            type="button"
-            onClick={()=>{
-              setCurrentStep(prev=>prev-1)
-            }}
+            <Button
+              className=" hover:bg-slate-800 "
+              variant="ghost"
+              type="button"
+              onClick={() => {
+                setCurrentStep((prev) => prev - 1);
+              }}
             >
               <ArrowLeft className="mr-1" /> Go Back
             </Button>
-            <Button
-            className="hover:bg-violet-600" variant="ghost"
-            >
+            <Button className="hover:bg-violet-600" variant="ghost">
               Next <ArrowRight className="ml-1" />
             </Button>
           </div>
