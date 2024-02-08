7260088ac51951b93194a632c45740117b048234
HurasAdam <huradam@wp.pl>
2024-02-08 23:58:34 +0100

    Add checkbox component, Refactor imports, add new API request, and configure query options.

--- frontend/src/_root/pages/CreateProject.tsx
+++ frontend/src/_root/pages/CreateProject.tsx
@@ -1,32 +1,49 @@
-import ProjectForm from '@/components/forms/projectForm/ProjectForm'
-import PostForm from '@/components/forms/projectForm/ProjectForm'
-import { getVisibilityOptions } from '@/features/project-api/project-api'
-import { getUsers } from '@/features/user-api/user-api'
-import React from 'react'
-import { useQuery } from 'react-query'
+import ProjectForm from "@/components/forms/projectForm/ProjectForm";
+import PostForm from "@/components/forms/projectForm/ProjectForm";
+import {
+  getTags,
+  getVisibilityOptions,
+} from "@/features/project-api/project-api";
+import { getUsers } from "@/features/user-api/user-api";
+import React from "react";
+import { useQuery } from "react-query";
 
 const CreateProject = () => {
+  const config = { refetchOnWindowFocus: false };
 
+  const {
+    data: users,
+    isLoading,
+    isError,
+  } = useQuery(["users"], () => getUsers(), config);
+  const { data: visibilityOptions } = useQuery(
+    ["visibilityOptions"],
+    () => getVisibilityOptions(),
+    config
+  );
+  const { data: tags } = useQuery(["tags"], () => getTags(), config);
 
-const {data:users,isLoading,isError}=useQuery(["users"],()=>getUsers())
-const {data:visibilityOptions}=useQuery(["visibilityOptions"],()=>getVisibilityOptions())
-  
   return (
-    <div className='flex flex-1'>
+    <div className="flex flex-1">
+      <div className="common-container ">
+        <div className="max-w-5xl w-full flex gap-3">
+          <img
+            src="/assets/icons/add-post.svg"
+            alt="add"
+            width={36}
+            height={36}
+          />
+          <h2 className="h3-bold  md:h2-bold  w-full">Create Project</h2>
+        </div>
 
-<div className='common-container '>
-
-<div className='max-w-5xl w-full flex gap-3'>
-  <img src="/assets/icons/add-post.svg" alt="add" width={36} height={36}/>
-  <h2 className='h3-bold  md:h2-bold  w-full'>Create Project</h2>
-</div>
-
-<ProjectForm users={users} visibilityOptions={visibilityOptions} />
-
-</div>
-
+        <ProjectForm
+          users={users}
+          visibilityOptions={visibilityOptions}
+          tags={tags}
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CreateProject
+export default CreateProject;
