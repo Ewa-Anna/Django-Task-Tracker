3beec0d3a09bd36df56bc26622ef7cae6beaf20e
HurasAdam <huradam@wp.pl>
2024-02-14 18:06:16 +0100

    Added DonutChart component.

--- frontend/src/components/ui/shared/DonutChart.tsx
+++ frontend/src/components/ui/shared/DonutChart.tsx
@@ -1,59 +1,170 @@
-import React from "react";
-import { PieChart, Pie, Legend } from "recharts";
+import React, { PureComponent, useRef, useState } from "react";
+import { IoIosArrowDown } from "react-icons/io";
+import {
+  PieChart,
+  Pie,
+  Sector,
+  Cell,
+  ResponsiveContainer,
+  Legend,
+} from "recharts";
 
-const DonutChart = () => {
+const theme = "dark";
+
+const DonutChart = ({ header }) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const [sort, setSort] = useState("overall");
+  const [isOpen, setIsOpen] = useState(false);
+console.log(sort)
+
   const data = [
-    { name: "Group A", value: 400, fill: "#0088FE" },
-    { name: "Group B", value: 300, fill: "#00C49F" },
-    { name: "Group C", value: 300, fill: "#FFBB28" },
-    { name: "Group D", value: 200, fill: "#FF8042" }
+    { name: "Bug", value: 400 },
+    { name: "Feature", value: 300 },
+    { name: "Question", value: 300 },
   ];
+  const COLORS = ["#881337", "#8d6333", "#0e7490",];
 
-  const renderColorfulLegendText = (value: string, entry: any) => {
+  const resort = (type) => {
+    setSort(type);
+    setIsOpen(false);
+  };
+
+  const RADIAN = Math.PI / 180;
+  const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    percent,
+    index,
+  }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.3;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
     return (
-      <span style={{ color: "#596579", fontWeight: 500, padding: "10px" }}>
-        {value}
-      </span>
+      <text
+        x={x}
+        y={y}
+        fill={theme === "dark" ? "white" : "orange"}
+        textAnchor={x > cx ? "start" : "end"}
+        dominantBaseline="central"
+      >
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
     );
   };
 
+const displaySortLabel= ()=>{
+let label
 
-    return (
-      <div className=" flex justify-center">
+if(sort==="last_month") label="Last Month"
+else if(sort==="last_week")label="Last Week"
+else if(sort==="today")label="Today"
+else if(sort==="overall")label="Overall"
+return label
+}
 
-<PieChart width={350} height={360} className=" ">
-  
-  <Pie
-    data={data}
-    cx="50%"
-    cy="50%"
-    innerRadius={60}
-    outerRadius={110}
-    fill="#8884d8"
-    paddingAngle={0}
-    dataKey="value"
-  >
+  return (
+    <div className=" w-full p-2  h-1/3 flex rounded-[4px]  flex-col border-2 border-dark-3">
+      <div className=" flex justify-between">
+        <h4 className="text-2xl mb-2">{header}</h4>
+        <div
+          className="flex  items-center gap-2 relative"
+          tabIndex={0}
+          ref={containerRef}
+          onBlur={() => {
+            setIsOpen(false);
+          }}
+          onClick={() => setIsOpen((state) => !state)}
+        >
+          <span className="mr-3 text-sm text-slate-300">sortBy</span>
+          <span className="text-sm text-slate-100 font-semibold border-2 w-[120px] py-1  flex justify-center  border-none bg-dark-3 rounded-[10px]">
+            {displaySortLabel()}
+          </span>
 
-  </Pie>
+          <div className="cursor-pointer hover:bg-dark-4 rounded-[25%] transition">
+            <IoIosArrowDown size={21} />
+          </div>
 
-  <Legend
-  className="border-2"
-    height={0}
-    width={0}
-    iconType="square"
-    layout="horizontal"
-    verticalAlign="bottom"
-    iconSize={15}
-    padding={40}
-    formatter={renderColorfulLegendText}
-  />
-</PieChart>
+          {isOpen && (
+            <div className="flex flex-col absolute top-10 cursor-pointer right-0 bg-dark-2 w-full p-2 gap-1 z-50">
+         {sort!=="overall"&&     <span
+                onClick={(e) => {
+                  e.stopPropagation();
+                  resort("overall");
+                }}
+                className="hover:text-violet-600"
+              >
 
+                Overall
+              </span>}
+              
+       {  sort!=="today"&&     <span
+                onClick={(e) => {
+                  e.stopPropagation();
+                  resort("today");
+                }}
+                className="hover:text-violet-600"
+              >
+                Today
+              </span>}
+          {  sort!=="last_week"&&  <span
+                className="hover:text-violet-600"
+              onClick={(e)=>{
+                e.stopPropagation();
+                resort("last_week")
+              }}
+              >Last Week</span>}
 
-   
+         {  sort!=="last_month"&&   <span
+                className="hover:text-violet-600"
+              onClick={(e)=>{
+                e.stopPropagation();
+                resort("last_month")
+              }}
+              >Last Month</span>}
 
+            </div>
+          )}
+        </div>
       </div>
-    )
+
+      <ResponsiveContainer width="90%" height="100%">
+        <PieChart width={400} height={400}>
+          <Legend
+            verticalAlign="middle"
+            align="right"
+            layout="vertical"
+            height={106}
+            iconSize={18}
+          />
+          <Pie
+            stroke="black"
+            isAnimationActive={true}
+            data={data}
+            cx="45%"
+            cy="50%"
+            labelLine={false}
+            label={renderCustomizedLabel}
+            outerRadius={110}
+            innerRadius={60}
+            fill="#8814d8"
+            dataKey="value"
+          >
+            {data.map((entry, index) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={COLORS[index % COLORS.length]}
+              />
+            ))}
+          </Pie>
+        </PieChart>
+      </ResponsiveContainer>
+    </div>
+  );
 };
 
 export default DonutChart;
