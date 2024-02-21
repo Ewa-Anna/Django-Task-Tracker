3b1e07b38d3eef2d1da2f7acd6e4bc1e04c4fe6c
HurasAdam <huradam@wp.pl>
2024-02-21 21:55:02 +0100

    Added optional time inclusion in formatTimestamp function.

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -5,7 +5,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export const formatTimestamp = (timestamp:string) => {
+export const formatTimestamp = (timestamp: string, includeTime: boolean = false): string => {
   const date = new Date(timestamp);
 
   const monthNamesShort = [
@@ -23,34 +23,13 @@ export const formatTimestamp = (timestamp:string) => {
     "Dec",
   ];
 
-  const formattedDate = `${
-    monthNamesShort[date.getMonth()]
-  } ${date.getDate()}, ${date.getFullYear()}`;
+  const formattedDate = `${monthNamesShort[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
+
+  if (includeTime) {
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    return `${formattedDate} ${hours}:${minutes}`;
+  }
 
   return formattedDate;
 };
-
-
-
-export function timeUntilDeadline(deadline:string):string {
-  // Aktualna data
-  const currentDate = new Date();
-  const deadlineTimestamp = new Date(deadline);
-  
-  // Obliczenie różnicy w milisekundach
-  const differenceInMilliseconds:number = deadlineTimestamp - currentDate;
- 
-  // Obliczenie liczby dni z różnicy w milisekundach
-  const differenceInDays = Math.floor(differenceInMilliseconds / (1000 * 60 * 60 * 24));
-
-  // Obliczenie reszty godzin, jeśli różnica wynosi mniej niż jeden dzień
-  const remainingHours = Math.floor((differenceInMilliseconds % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-
-  // Zwracanie odpowiedniej formy
-  if (differenceInDays > 0) {
-      return `${differenceInDays} days`;
-  } else {
-      return `${remainingHours} hours`;
-  }
-}
-
