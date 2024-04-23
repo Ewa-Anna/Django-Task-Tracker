a41e1ce20b34b3057e598266ca841e9117188ab4
HurasAdam <huradam@wp.pl>
2024-04-23 20:11:50 +0200

    Feat: Added Register and handle Register form.

--- frontend/src/constants/images.ts
+++ frontend/src/constants/images.ts
@@ -1,18 +1,16 @@
 import Logo from "../assets/logo_4.svg";
 
-import ProfileImage from "../assets/icons/user.svg"
-import ProjectImage from "../assets/projectImage.svg"
-import LandingImage from "../assets/LandingImage.svg"
-
+import ProfileImage from "../assets/icons/user.svg";
+import ProjectImage from "../assets/projectImage.svg";
+import LandingImage from "../assets/LandingImage.svg";
+import RegisterImage from "../assets/registerImage.svg";
 
 const images = {
-    Logo,
-    LandingImage,
-    ProfileImage,
-    ProjectImage
-
-
-}
+  Logo,
+  LandingImage,
+  ProfileImage,
+  ProjectImage,
+  RegisterImage,
+};
 
 export default images;
-
--- frontend/src/services/projectsApi.ts
+++ frontend/src/services/projectsApi.ts
@@ -1,129 +1,144 @@
-import axios from "axios"
+import axios from "axios";
 import clientApi from "../axios";
 
-export const getAllProjects = async ({ limit = 12, title = "", offset = 0, }) => {
+export const getAllProjects = async ({
+  limit = 12,
+  title = "",
+  offset = 0,
+}) => {
+  try {
+    const config = {
+      withCredentials: true,
+    };
 
-    try {
-        const config = {
-            withCredentials: true,
-        }
+    const response = await axios.get(
+      `http://127.0.0.1:8000/task-tracker/v1/task/projects/?title=${title}&offset=${offset - 1 * limit}&limit=${limit}`,
+      config
+    );
 
-        const response = await axios.get(`http://127.0.0.1:8000/task-tracker/v1/task/projects/?title=${title}&offset=${offset - 1 * limit}&limit=${limit}`, config)
-
-        return response.data;
-    } catch (error) {
-        console.log(error)
-        if (axios.isAxiosError(error)) {
-            console.log("error message:", error.message)
-            throw new Error(error.message);
-        } else {
-            console.log("undexpected error", error)
-            throw new Error("An unexpected error ocured")
-        }
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    if (axios.isAxiosError(error)) {
+      console.log("error message:", error.message);
+      throw new Error(error.message);
+    } else {
+      console.log("undexpected error", error);
+      throw new Error("An unexpected error ocured");
     }
-}
+  }
+};
 
 export const getProjectDetails = async ({ id }) => {
+  try {
+    const config = {
+      withCredentials: true,
+    };
 
-    try {
-        const config = {
-            withCredentials: true,
-        }
+    const response = await axios.get(
+      `http://127.0.0.1:8000/task-tracker/v1/task/projects/${id}`,
+      config
+    );
 
-        const response = await axios.get(`http://127.0.0.1:8000/task-tracker/v1/task/projects/${id}`, config)
-
-        return response.data;
-    } catch (error) {
-        console.log(error)
-        if (axios.isAxiosError(error)) {
-            console.log("error message:", error.message)
-            throw new Error(error.message);
-        } else {
-            console.log("undexpected error", error)
-            throw new Error("An unexpected error ocured")
-        }
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    if (axios.isAxiosError(error)) {
+      console.log("error message:", error.message);
+      throw new Error(error.message);
+    } else {
+      console.log("undexpected error", error);
+      throw new Error("An unexpected error ocured");
     }
-}
-
+  }
+};
 
 export const getTags = async () => {
+  try {
+    const config = {
+      withCredentials: true,
+    };
 
-    try {
-        const config = {
-            withCredentials: true,
-        }
+    const response = await axios.get(
+      "http://127.0.0.1:8000/task-tracker/v1/tags/",
+      config
+    );
 
-        const response = await axios.get("http://127.0.0.1:8000/task-tracker/v1/tags/", config)
-
-        return response.data;
-    } catch (error) {
-        console.log(error)
-        if (axios.isAxiosError(error)) {
-            console.log("error message:", error.message)
-            throw new Error(error.message);
-        } else {
-            console.log("undexpected error", error)
-            throw new Error("An unexpected error ocured")
-        }
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    if (axios.isAxiosError(error)) {
+      console.log("error message:", error.message);
+      throw new Error(error.message);
+    } else {
+      console.log("undexpected error", error);
+      throw new Error("An unexpected error ocured");
     }
-}
+  }
+};
 
 export const getVisibilityOptions = async () => {
-    try {
-        const config = {
-            withCredentials: true,
-        }
+  try {
+    const config = {
+      withCredentials: true,
+    };
 
-        const response = await axios.get("http://127.0.0.1:8000/task-tracker/v1/task/dropdown-list/visibility", config)
+    const response = await axios.get(
+      "http://127.0.0.1:8000/task-tracker/v1/task/dropdown-list/visibility",
+      config
+    );
 
-        return response.data;
-    } catch (error) {
-        console.log(error)
-        if (axios.isAxiosError(error)) {
-            console.log("error message:", error.message)
-            throw new Error(error.message);
-        } else {
-            console.log("undexpected error", error)
-            throw new Error("An unexpected error ocured")
-        }
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    if (axios.isAxiosError(error)) {
+      console.log("error message:", error.message);
+      throw new Error(error.message);
+    } else {
+      console.log("undexpected error", error);
+      throw new Error("An unexpected error ocured");
     }
-}
+  }
+};
 
 export const createNewProject = async ({ formData }) => {
-    try {
+  try {
+    const response = await clientApi.post(
+      "http://127.0.0.1:8000/task-tracker/v1/task/projects/",
+      formData
+    );
 
-
-        const response = await clientApi.post("http://127.0.0.1:8000/task-tracker/v1/task/projects/", formData)
-
-        return response.data;
-    } catch (error) {
-        console.log(error)
-        if (axios.isAxiosError(error)) {
-            console.log("error message:", error.message)
-            throw new Error(error.message);
-        } else {
-            console.log("undexpected error", error)
-            throw new Error("An unexpected error ocured")
-        }
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    if (axios.isAxiosError(error)) {
+      console.log("error message:", error.message);
+      throw new Error(error.message);
+    } else {
+      console.log("undexpected error", error);
+      throw new Error("An unexpected error ocured");
     }
-}
-
+  }
+};
 
 export const updateProject = async ({ formData }) => {
-    try {
-        const projectId = formData.get("id");
+  try {
+    const projectId = formData.get("id");
 
-        const response = await clientApi.patch(`http://127.0.0.1:8000/task-tracker/v1/task/projects/${projectId}/`, formData)
+    const response = await clientApi.patch(
+      `http://127.0.0.1:8000/task-tracker/v1/task/projects/${projectId}/`,
+      formData
+    );
 
-        return response.data;
-    } catch (error) {
-        console.log(error)
-        if (axios.isAxiosError(error)) {
-            console.log("error message:", error.message)
-            throw new Error(error.message);
-        } else {
-            console.log("undexpected error", error)
-            throw new Error("An unexpected error ocured")
-        }
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    if (axios.isAxiosError(error)) {
+      console.log("error message:", error.message);
+      throw new Error(error.message);
+    } else {
+      console.log("undexpected error", error);
+      throw new Error("An unexpected error ocured");
     }
-}
+  }
+};
